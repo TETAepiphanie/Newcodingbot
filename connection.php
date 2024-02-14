7b2496cddffd74f9ce8b8f1459c47720da061785
my all <159054252+TETAepiphanie@users.noreply.github.com>
2024-02-14 11:11:14 +0200

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+$conn=mysqli_connect('localhost','root','','l4software and development');
+//  if($conn){
+//  	echo "<script>
+//  	alert('success');
+// 	</script>";
+//  }
+//  else{
+//  	echo "<script>
+//  	alert('failed to connect');
+//  	</script>";
+// }
+?>
